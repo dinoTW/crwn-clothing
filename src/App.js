4e2040889cbe33dd925d6d33b8edb820a012d5de
dinozzosue <ricky322655@yahoo.com.tw>
2021-10-21 20:28:28 +0800

    update our menu-iten to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+
+import { Route } from 'react-router-dom'
+
 import Homepage from './pages/Homepage/Homepage';
+
+const HatPage = () => {
+  return (
+    <h1>Topic Detail Page</h1>
+  );
+}
 
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Route exact path="/" component={ Homepage } />
+      <Route path="/shop/hats" component={ HatPage } />     
     </div>
   );
 }
